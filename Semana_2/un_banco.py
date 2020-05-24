ab8f21277667ffa82c769cf3769b05b850967c70
MONICA ANDREA ANDREA <1719110195@utectulancingo.edu.mx>
2020-05-24 16:45:25 +0000

    un_banco.py

---
+++ Semana_2/un_banco.py
@@ -0,0 +1,48 @@
+class Banco:
+
+  #Atributos
+  nombre="Bancoppel"
+  tamano="50 m2"
+  logo="el nombre delbanco y una llave invertida"
+  eslogan="coppel mejora tu vida"
+  diseño="cuadrado"
+  horario="9:00 a 19:30 horas"
+  color="amarillo"
+  servicios="retiros,prestamos, pagos de servicios "
+  dirección="cualquier tiend coppel"
+  estacionamiento="planta baja"
+
+  #Metodos
+  def retirar dinero (self):
+    print ("Retirar dinero")
+  def depositar (self):
+    print("depositar")
+  def hacer prestamos (self):
+    print("hacer prestamos")
+  def pagar (self):
+    print ("pagar")
+  def cobrar (self):
+    print ("cobrar")
+
+  def __init__(self)
+    print ("metodos banco")
+
+
+objeto_banco= banco ()
+
+objeto_banco.retirar()
+objeto_banco.depositar()
+objeto_banco.hacerprestamos()
+objeto_banco.pagar()
+objeto_banco.cobrar()
+
+print (objeto_banco.nombre)
+print (objeto_banco.tamano)
+print (objeto_banco.logo)
+print (objeto_banco).eslogan
+print (objeto_banco).diseno)
+print (objeto_banco.horario)
+print (objeto_banco.color)
+print (objeto_banco.servicios)
+print (objeto_banco.direccion)
+print (objeto_banco.estacionamiento)
