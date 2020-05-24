8593852d163bcb9ca16893a710be377906ba1153
MONICA ANDREA ANDREA <1719110195@utectulancingo.edu.mx>
2020-05-24 17:23:16 +0000

    un_banco.py

--- Semana_2/un_banco.py
+++ Semana_2/un_banco.py
@@ -5,44 +5,45 @@ class Banco:
   tamano="50 m2"
   logo="el nombre delbanco y una llave invertida"
   eslogan="coppel mejora tu vida"
-  diseño="cuadrado"
+  diseno="cuadrado"
   horario="9:00 a 19:30 horas"
   color="amarillo"
   servicios="retiros,prestamos, pagos de servicios "
-  dirección="cualquier tiend coppel"
+  direccion="cualquier tiend coppel"
   estacionamiento="planta baja"
 
   #Metodos
-  def retirar dinero (self):
+  def retirar_dinero (self):
     print ("Retirar dinero")
   def depositar (self):
     print("depositar")
-  def hacer prestamos (self):
+  def hacer_prestamos (self):
     print("hacer prestamos")
   def pagar (self):
     print ("pagar")
   def cobrar (self):
     print ("cobrar")
 
-  def __init__(self)
+  def __init__(self):
     print ("metodos banco")
 
 
-objeto_banco= banco ()
+objeto_un_banco= Banco()
 
-objeto_banco.retirar()
-objeto_banco.depositar()
-objeto_banco.hacerprestamos()
-objeto_banco.pagar()
-objeto_banco.cobrar()
+objeto_un_banco.retirar_dinero()
+objeto_un_banco.depositar()
+objeto_un_banco.hacer_prestamos()
+objeto_un_banco.pagar()
+objeto_un_banco.cobrar()
 
-print (objeto_banco.nombre)
-print (objeto_banco.tamano)
-print (objeto_banco.logo)
-print (objeto_banco).eslogan
-print (objeto_banco).diseno)
-print (objeto_banco.horario)
-print (objeto_banco.color)
-print (objeto_banco.servicios)
-print (objeto_banco.direccion)
-print (objeto_banco.estacionamiento)
+
+print (objeto_un_banco.nombre)
+print (objeto_un_banco.tamano)
+print (objeto_un_banco.logo)
+print (objeto_un_banco.eslogan)
+print (objeto_un_banco.diseno)
+print (objeto_un_banco.horario)
+print (objeto_un_banco.color)
+print (objeto_un_banco.servicios)
+print (objeto_un_banco.direccion)
+print (objeto_un_banco.estacionamiento)
